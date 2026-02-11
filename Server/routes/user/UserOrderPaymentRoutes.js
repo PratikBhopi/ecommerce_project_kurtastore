63764966888c2dd6c71e11931a2ac12ea53cc88a
Pratik Bhopi <pkbhopi132@gmail.com>
2026-02-12 01:29:17 +0530

    improvements:fix

--- Server/routes/user/UserOrderPaymentRoutes.js
+++ Server/routes/user/UserOrderPaymentRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express')  
-const { order } = require('../../controllers/user/Products')
+const express = require('express')
+const { order } = require('../../controllers/user/OrderController')
 const { handlePayment, createOrder, paymentOrder, getOrders } = require('../../controllers/user/PaymentController')
 
 const UserOrderPaymentRouter = express.Router()
 
-UserOrderPaymentRouter.post('/submitOrderDetails',order)
-UserOrderPaymentRouter.post('/createOrder',createOrder)
-UserOrderPaymentRouter.post('/payment-success',paymentOrder)
-UserOrderPaymentRouter.get('/getorders',getOrders)
+UserOrderPaymentRouter.post('/submitOrderDetails', order)
+UserOrderPaymentRouter.post('/createOrder', createOrder)
+UserOrderPaymentRouter.post('/payment-success', paymentOrder)
+UserOrderPaymentRouter.get('/getorders', getOrders)
 
 module.exports = UserOrderPaymentRouter
